a8c5b6363a7da2f0926a96b6769c10d4b63c0ca6
Yakimov Lev <tiokuhytblu@gmail.com>
2023-06-21 19:58:40 +0500

    Добавьте файлы проекта.

---
+++ Eleventh_Lesson/Eleventh_Lesson.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "log_duration.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    {
+        LOG_DURATION("Прединкремент.");
+        for (int i = 0; i < 1000000000; ++i)
+        {
+
+        }
+    }
+
+    {
+        LOG_DURATION("Постинкремент.");
+        for (int i = 0; i < 1000000000; i++)
+        {
+
+        }
+    }
+}
+
+
